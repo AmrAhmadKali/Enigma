dbaa4d68d61e6154d9ff11789d5719745c304fac
Martin Wallinger <martin.wallinger@stud.th-deg.de>
2023-05-11 21:36:06 +0200

    plugboard zwischenstand

--- frontend/src/js/Plugboard.js
+++ frontend/src/js/Plugboard.js
@@ -1,30 +1,39 @@
 var letters = []
 var lastpressed = null
 function plugboard_pressed(key) {
-
     if (lastpressed == null) {
         lastpressed = key
+        $('.plugboardContainer').append("<div id='lastpressed'></div><br>")
+        $('#lastpressed').append(key)
         return
 
     }
-    if (lastpressed == key) {
+    if (lastpressed === key) {
         lastpressed = null
+        $('#lastpressed').remove()
         return
     }
 
     for (let i = 0; i < letters.length; i++) {
         if (letters[i].includes(key)) {
             letters.splice(i, 1)
+            const reg = new RegExp(".?"+key+".?")
+            // for(element in '.plugboardContainer'){
+            //     exec(element)
+            //         ...
+            // }
+            // $('.plugboardContainer').remove("."+...)
+            // TODO: remove Abfrage
             return
         }
-
     }
 
     letters.push([key, lastpressed])
     //awaiting.push('plugboard_set')
     //sendRequest('plugboard', 'set', [key, lastpressed])
+    $('.plugboardContainer').append("<div id='"+key+lastpressed+"'>"+key+lastpressed+"</div><br>")
     lastpressed = null
-    console.log("Plugboardeinstellung"+ letters)
+    $('#lastpressed').remove()
 }
 
 function reset_plugboard() {
@@ -32,6 +41,6 @@ function reset_plugboard() {
     letters = null
     //awaiting.push('plugboard_reset')
     //sendRequest('plugboard', 'reset')
-
+    $('.plugboardContainer').empty()
 }
 
