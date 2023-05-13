f8780f614ea1867468f2887bf9c7a85fa90ded4d
Martin Wallinger <martin.wallinger@stud.th-deg.de>
2023-05-13 10:58:40 +0200

    completed plugboard logic

--- frontend/src/js/Plugboard.js
+++ frontend/src/js/Plugboard.js
@@ -1,46 +1,50 @@
 var letters = []
 var lastpressed = null
 function plugboard_pressed(key) {
+       for (let i = 0; i < letters.length; i++) {
+        if (letters[i].includes(key)) {
+            letters.splice(i, 1)
+            const reg = new RegExp(".?" + key + ".?")
+            const divs = [...document.getElementsByClassName('tuple')];
+            for (let i in divs) {
+                if (reg.test(divs[i].outerText)) {
+                    awaiting.push('plugboard_set')
+                    sendRequest('plugboard', 'set', divs[i].outerText)
+                    divs[i].remove()
+                    return
+                }
+            }
+            return
+        }
+    }
+
+
     if (lastpressed == null) {
         lastpressed = key
         $('.plugboardContainer').append("<div id='lastpressed'></div><br>")
         $('#lastpressed').append(key)
         return
-
     }
+
     if (lastpressed === key) {
         lastpressed = null
         $('#lastpressed').remove()
         return
     }
 
-    for (let i = 0; i < letters.length; i++) {
-        if (letters[i].includes(key)) {
-            letters.splice(i, 1)
-            const reg = new RegExp(".?"+key+".?")
-            // for(element in '.plugboardContainer'){
-            //     exec(element)
-            //         ...
-            // }
-            // $('.plugboardContainer').remove("."+...)
-            // TODO: remove Abfrage
-            return
-        }
-    }
-
     letters.push([key, lastpressed])
-    //awaiting.push('plugboard_set')
-    //sendRequest('plugboard', 'set', [key, lastpressed])
-    $('.plugboardContainer').append("<div id='"+key+lastpressed+"'>"+key+lastpressed+"</div><br>")
+    awaiting.push('plugboard_set')
+    sendRequest('plugboard', 'set', [key, lastpressed])
+    $('.plugboardContainer').append("<div class='tuple' id='"+key+lastpressed+"'>"+key+lastpressed+"</div><br>")
     lastpressed = null
     $('#lastpressed').remove()
 }
 
 function reset_plugboard() {
     lastpressed = null
-    letters = null
-    //awaiting.push('plugboard_reset')
-    //sendRequest('plugboard', 'reset')
+    letters = []
+    awaiting.push('plugboard_reset')
+    sendRequest('plugboard', 'reset')
     $('.plugboardContainer').empty()
 }
 
