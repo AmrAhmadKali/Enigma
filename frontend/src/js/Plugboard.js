2d107b1364b108726f479eaf0886b0fc664f96dd
mw10270 <martin.wallinger@stud.th-deg.de>
2023-05-15 17:43:43 +0200

    fix plugboardContainer

--- frontend/src/js/Plugboard.js
+++ frontend/src/js/Plugboard.js
@@ -1,41 +1,38 @@
 var letters = []
 var lastpressed = null
+
 function plugboard_pressed(key) {
        for (let i = 0; i < letters.length; i++) {
         if (letters[i].includes(key)) {
             letters.splice(i, 1)
             const reg = new RegExp(".?" + key + ".?")
-            const divs = [...document.getElementsByClassName('tuple')];
-            for (let i in divs) {
-                if (reg.test(divs[i].outerText)) {
+            const elements = [...document.getElementsByClassName('tuple')];
+            for (let i in elements) {
+                if (reg.test(elements[i].outerText)) {
                     awaiting.push('plugboard_set')
-                    sendRequest('plugboard', 'set', divs[i].outerText)
-                    divs[i].remove()
+                    sendRequest('plugboard', 'set', elements[i].outerText)
+                    elements[i].remove()
                     return
                 }
             }
             return
         }
     }
-
-
     if (lastpressed == null) {
         lastpressed = key
-        $('.plugboardContainer').append("<div id='lastpressed'></div><br>")
+        $('.plugboardContainer').append("<p id='lastpressed'></p>")
         $('#lastpressed').append(key)
         return
     }
-
     if (lastpressed === key) {
         lastpressed = null
         $('#lastpressed').remove()
         return
     }
-
     letters.push([key, lastpressed])
     awaiting.push('plugboard_set')
     sendRequest('plugboard', 'set', ''+key+lastpressed+'')
-    $('.plugboardContainer').append("<div class='tuple' id='"+key+lastpressed+"'>"+key+lastpressed+"</div><br>")
+    $('.plugboardContainer').append("<p class='tuple' id='"+key+lastpressed+"'>"+lastpressed+key+"</p><p>,&nbsp;</p>")
     lastpressed = null
     $('#lastpressed').remove()
 }
@@ -47,4 +44,3 @@ function reset_plugboard() {
     sendRequest('plugboard', 'reset')
     $('.plugboardContainer').empty()
 }
-
