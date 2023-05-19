3e20b28361170e712444d27abfd0daf42a17554f
Martin Wallinger <martin.wallinger@stud.th-deg.de>
2023-05-19 14:05:52 +0200

    10 plugs Limit + colors added

--- frontend/src/js/Plugboard.js
+++ frontend/src/js/Plugboard.js
@@ -2,7 +2,7 @@ var letters = []
 var lastpressed = null
 
 function plugboard_pressed(key) {
-       for (let i = 0; i < letters.length; i++) {
+    for (let i = 0; i < letters.length; i++) {
         if (letters[i].includes(key)) {
             letters.splice(i, 1)
             const reg = new RegExp(".?" + key + ".?")
@@ -12,29 +12,40 @@ function plugboard_pressed(key) {
                     awaiting.push('plugboard_set')
                     sendRequest('plugboard', 'set', elements[i].outerText)
                     elements[i].remove()
+                    $('[name="p_'+elements[i].outerText[0]+'"]').css('background-color', 'white')
+                    $('[name="p_'+elements[i].outerText[1]+'"]').css('background-color', 'white')
                     return
                 }
             }
             return
         }
     }
+    if (checkPlugLimitReached()){
+        alert("Only 10 Plugs are allowed")
+        return
+    }
     if (lastpressed == null) {
         lastpressed = key
         $('.plugboardContainer').append("<p id='lastpressed'></p>")
         $('#lastpressed').append(key)
+        $('[name="p_'+key+'"]').css('background-color', 'LightCoral')
         return
     }
     if (lastpressed === key) {
-        lastpressed = null
         $('#lastpressed').remove()
+        $('[name="p_'+key+'"]').css('background-color', 'white')
+        lastpressed = null
         return
     }
     letters.push([key, lastpressed])
     awaiting.push('plugboard_set')
     sendRequest('plugboard', 'set', ''+key+lastpressed+'')
     $('.plugboardContainer').append("<p class='tuple' id='"+key+lastpressed+"'>"+lastpressed+key+',&nbsp;'+"</p>")
-    lastpressed = null
     $('#lastpressed').remove()
+    $('[name="p_'+key+'"]').css('background-color', 'LightBlue')
+    $('[name="p_'+lastpressed+'"]').css('background-color', 'LightBlue')
+    lastpressed = null
+
 }
 
 function reset_plugboard() {
@@ -43,4 +54,17 @@ function reset_plugboard() {
     awaiting.push('plugboard_reset')
     sendRequest('plugboard', 'reset')
     $('.plugboardContainer').empty()
+}
+
+function checkPlugLimitReached(){
+    let plugboardContainer = document.querySelector(".plugboardContainer");
+    let contentLength = plugboardContainer.textContent.length
+
+    if (contentLength%4 === 0) {
+        let plugs = contentLength / 4
+        if (plugs === 10){
+            return true
+        }
+    }
+    return false
 }
