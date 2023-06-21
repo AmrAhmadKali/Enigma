b380fbffe5e30e3d7c2a5f2bd914cbc1eb02ab85
AmrAhmadKali <69507462+AmrAhmadKali@users.noreply.github.com>
2023-06-21 19:06:18 +0200

    delete waits, E2E to plugboard existence and rotrs offsets

--- E2E/steps/website-navigation-steps.py
+++ E2E/steps/website-navigation-steps.py
@@ -20,7 +20,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def step_impl(context):
     # spin up driver
     options = Options()
-    options.headless = False  # To change
+    options.headless = True  # To change
     context.driver = webdriver.Firefox(options=options, service=Service(GeckoDriverManager().install()))
     context.action_chains = ActionChains(context.driver)
     if "CI" in os.environ.keys():
